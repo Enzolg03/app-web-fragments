7de8d7159d456235182dd8f800ebf309a8311a0f
goshaaaa777 <isaiasddx322z@gmail.com>
2023-10-01 09:47:44 -0500

    primer commit

---
+++ src/test/java/com/cibertec/edu/pe/appwebfragments/AppWebFragmentsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cibertec.edu.pe.appwebfragments;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppWebFragmentsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
